231a1ab5b365ae0416677b06bae2a4b87197be2c
Steven Haryanto <stevenharyanto@gmail.com>
2013-11-08 06:39:36 +0000

    Import of SHARYANTO/App-norepeat-0.01 from CPAN.
    gitpan-cpan-distribution: App-norepeat
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/App-norepeat-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/App/norepeat.pm
@@ -0,0 +1,61 @@
+package App::norepeat;
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: Run commands, but not repeatedly
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+App::norepeat - Run commands, but not repeatedly
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+See the command-line script L<norepeat>.
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+
+None are exported by default, but they are exportable.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/App-norepeat>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-App-norepeat>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=App-norepeat>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
